87675de6deae28b8c3939060b34ecd3f055fffd7
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-03-05 19:59:02 +0300

    Modify exercise-6.py

--- lesson-3/exercise-6/my_venv/exercise-6.py
+++ lesson-3/exercise-6/my_venv/exercise-6.py
@@ -6,3 +6,17 @@
 и максимальный элементы в сумму не включать.
 '''
 
+from random import randint
+
+my_list = [randint(1, 100) for _ in range(10)]
+print(f'Список: {my_list}')
+
+if my_list.index(min(my_list)) < my_list.index(max(my_list)):
+    a = my_list.index(min(my_list)) + 1
+    b = my_list.index(max(my_list))
+else:
+    a = my_list.index(max(my_list)) + 1
+    b = my_list.index(min(my_list))
+
+print(f'Элементы находящиеся между минимальным и максимальнм элементами: {sum(my_list[a:b])}')
+
