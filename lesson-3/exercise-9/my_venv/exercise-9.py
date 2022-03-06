4ed6e28e4a9da54aafb42041d9d0b65fba9951e8
Aleksandr <alexander_sheykin@mail.ru>
2022-03-06 21:33:37 +0300

    Modify exercise-9.py

--- lesson-3/exercise-9/my_venv/exercise-9.py
+++ lesson-3/exercise-9/my_venv/exercise-9.py
@@ -4,3 +4,21 @@
 '''
 9. Найти максимальный элемент среди минимальных элементов столбцов матрицы.
 '''
+from random import randint
+
+my_list = [[randint(1,20) for i in range(0,5)] for j in range(0,4)]
+for i in my_list:
+    print(i)
+
+my_var_1 = 0
+for j in range(0,5):
+    my_var_2 = 21
+    for i in range(0,4):
+        if my_list[i][j] < my_var_2:
+            my_var_2 = my_list[i][j]
+        else: continue
+    if my_var_2 > my_var_1:
+        my_var_1 = my_var_2
+
+print(f'Максимальный элемент среди минимальных элементов столбцов матрицы: {my_var_1}')
+
