a9225d6dba9d9b3c3252f3b26063dc5e670a30b3
Aleksandr <alexander_sheykin@mail.ru>
2022-03-28 22:33:49 +0300

    Modify exercise-2.py

--- lesson-7/exercise-2/my_venv/exercise-2.py
+++ lesson-7/exercise-2/my_venv/exercise-2.py
@@ -7,8 +7,34 @@
 '''
 from random import randrange
 
+def merge(left,right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:] + right[j:]
+    return result
+
+
+
+def merge_sort(array):
+    middle = len(array) // 2
+    left = array[:middle]
+    right = array[middle:]
+    if len(left) > 1:
+        left = merge_sort(left)
+    if len(right) > 1:
+        right = merge_sort(right)
+    return merge(left,right)
 
 if __name__ == '__main__':
     my_list = [None] * 10
     for i in range(len(my_list)):
         my_list[i] = randrange(0,50)
+    print(my_list)
+    print(merge_sort(my_list))
