dee1aeb70041f1496a2a4d6716c46ba9aed9de00
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-03-29 15:39:09 +0300

    Modify exercise-3.py

--- lesson-7/exercise-3/my_venv/exercise-3.py
+++ lesson-7/exercise-3/my_venv/exercise-3.py
@@ -7,7 +7,42 @@
 – не больше медианы. Задачу можно решить без сортировки исходного массива. Но если это слишком сложно, то используйте
 метод сортировки, который не рассматривался на уроках.
 '''
-from random import randrange
+from random import randrange,choice
+from statistics import median
+from timeit import timeit
+
+def my_func_1(array):
+    return median(array)
+
+# ----------------------------------------------#
+
+def my_func_2(array):
+    array.sort()
+    middle = len(array) // 2
+    return (array[middle] + array[middle]) / 2
+
+# ----------------------------------------------#
+
+def my_func_3(array,pivot_func = choice):
+    if len(array) % 2 == 1:
+        return my_func_31(array, len(array)/2, pivot_func)
+    else:
+        return 0.5 * (my_func_31(array, len(array)/2-1,pivot_func) +
+                      my_func_31(array, len(array)/2, pivot_func))
+
+def my_func_31(array,idx,pivot_func):
+    if len(array) == 1:
+        return array[0]
+    pivot = pivot_func(array)
+    l_list = [i for i in array if i <  pivot]
+    r_list = [i for i in array if i >  pivot]
+    p_list = [i for i in array if i == pivot]
+    if idx < len(l_list):
+        return my_func_31(l_list,idx,pivot_func)
+    elif idx < len(l_list) + len(p_list):
+        return p_list[0]
+    else:
+        return my_func_31(r_list, idx - len(l_list) - len(p_list), pivot_func)
 
 
 if __name__ == '__main__':
@@ -16,4 +51,16 @@ if __name__ == '__main__':
     my_list = [None] * size
     for i in range(len(my_list)):
         my_list[i] = randrange(-100,100)
-    print(my_list)
+    print(f'Исходный массив: {my_list}\n'
+          f'Медиана(Способ №1): {my_func_1(my_list)}\n'
+          f'''Время выполнения: {timeit(stmt="my_func_1(my_list)",
+                                        setup="from __main__ import my_func_1, my_list",
+                                        number=1000)} мс\n'''
+          f'Медиана(Способ №2): {my_func_2(my_list)}\n'
+          f'''Время выполнения: {timeit(stmt="my_func_2(my_list)",
+                                        setup="from __main__ import my_func_2, my_list",
+                                        number=1000)} мс\n'''
+          f'Медиана(Способ №3): {my_func_3(my_list)}\n'
+          f'''Время выполнения: {timeit(stmt="my_func_3(my_list)",
+                                        setup="from __main__ import my_func_3, my_list",
+                                        number=1000)} мс''')
