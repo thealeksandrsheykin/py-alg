66e3cba2224d17482d80e1b42a17ed468fce63dd
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-04-04 19:20:02 +0300

    Create exercise-2.py

--- lesson-8/exercise-2/my_venv/exercise-2.py
+++ lesson-8/exercise-2/my_venv/exercise-2.py
@@ -4,3 +4,13 @@
 '''
 2.Закодировать любую строку по алгоритму Хаффмана
 '''
+
+import heapq
+from collections import Counter,namedtuple
+
+
+class Node:
+    def __init__(namedtuple('Node',['left','right'])):
+        self.right = right
+        self.left = left
+        self.value = value
