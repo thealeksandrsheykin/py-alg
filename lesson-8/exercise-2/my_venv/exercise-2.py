a43963a23ab441f200d8a848be8b4440bac2d9b2
Aleksandr <alexander_sheykin@mail.ru>
2022-04-04 23:40:05 +0300

    Modify exercise-2.py

--- lesson-8/exercise-2/my_venv/exercise-2.py
+++ lesson-8/exercise-2/my_venv/exercise-2.py
@@ -5,30 +5,62 @@
 2.Закодировать любую строку по алгоритму Хаффмана
 '''
 
-from heapq import heapify, heappop, heappush
-from collections import namedtuple, Counter
+from collections import Counter
 
 
-class Node(namedtuple('Node', ['left', 'right'])):
-    def walk(self, code, acc):
-        self.left.walk(code, acc + '0')
-        self.right.walk(code, acc + '1')
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.right = right
+        self.left = left
+        self.value = value
 
 
-class Leaf(namedtuple('Leaf', ['char'])):
-    def walk(self, code, acc):
-        code[self.char] = acc or '0'
+def get_code(head, codes=dict(), code=''):
+    if head is None:
+        return None
+    if isinstance(head.value, str):
+        codes[head.value] = code
+        return codes
+    get_code(head.left,  codes, code+'0')
+    get_code(head.right, codes, code+'1')
+
+    return codes
+
+def get_tree(string):
+    string_count = Counter(string)
+
+    if len(string_count) <= 1:
+        node = Node(None)
+        if len(string_count) == 1:
+            node.left = Node(string_count[0])
+            node.right = Node(None)
+        string_count = {node: 1}
 
 
-def huffman_encode(data):
-    queue = list()
-    for char, freq in Counter(s).items():
-        queue.append((freq, len(queue), Leaf(char)))
-    heapify(queue)
+    while len(string_count) != 1:
+        node = Node(None)
+        spam = string_count.most_common()[:-3:-1]
+        if isinstance(spam[0][0], str):
+            node.left = Node(spam[0][0])
+        else:
+            node.left = spam[0][0]
+        if isinstance(spam[1][0], str):
+            node.right = Node(spam[1][0])
+        else:
+            node.right = spam[1][0]
+        del string_count[spam[0][0]]
+        del string_count[spam[1][0]]
+        string_count[node] = spam[0][1] + spam[1][1]
+    return [i for i in string_count][0]
 
 
 
 
 if __name__ == '__main__':
     s = input('Введите строку: ')
-    code = huffman_encode(s)
+    tree = get_tree(s)
+    code = get_code(tree)
+    result = ''
+    for i in s:
+        result += code[i]
+    print(f'Закодированная строка {s}: {result}')
