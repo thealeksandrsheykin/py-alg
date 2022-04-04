7ff8fe35d15fb283f04be7789ebab1fa3049cbea
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-04-04 20:22:23 +0300

    Modify exercise-2.py

--- lesson-8/exercise-2/my_venv/exercise-2.py
+++ lesson-8/exercise-2/my_venv/exercise-2.py
@@ -5,12 +5,30 @@
 2.Закодировать любую строку по алгоритму Хаффмана
 '''
 
-import heapq
-from collections import Counter,namedtuple
+from heapq import heapify, heappop, heappush
+from collections import namedtuple, Counter
 
 
-class Node:
-    def __init__(namedtuple('Node',['left','right'])):
-        self.right = right
-        self.left = left
-        self.value = value
+class Node(namedtuple('Node', ['left', 'right'])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + '0')
+        self.right.walk(code, acc + '1')
+
+
+class Leaf(namedtuple('Leaf', ['char'])):
+    def walk(self, code, acc):
+        code[self.char] = acc or '0'
+
+
+def huffman_encode(data):
+    queue = list()
+    for char, freq in Counter(s).items():
+        queue.append((freq, len(queue), Leaf(char)))
+    heapify(queue)
+
+
+
+
+if __name__ == '__main__':
+    s = input('Введите строку: ')
+    code = huffman_encode(s)
